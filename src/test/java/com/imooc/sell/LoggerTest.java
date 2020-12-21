d761dc5a067111a4959821bfc169e1a365e7e169
wdd <15204659487@163.com>
2020-12-21 08:07:53 +0800

    master

---
+++ src/test/java/com/imooc/sell/LoggerTest.java
@@ -0,0 +1,13 @@
+package imooc.sell;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class LoggerTest {
+//
+//    private final Logger
+//
+//    @Test
+//    public void test1(){
+//
+//    }
+//}
